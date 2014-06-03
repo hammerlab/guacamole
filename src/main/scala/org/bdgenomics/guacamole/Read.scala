1a9697b007f3ecdd892afab561349165e7673016
Tim O'Donnell <timodonnell@gmail.com>
2014-06-03 16:25:14 -0400

    Save a few bytes in the serializer for (Un)mappedRead

--- src/main/scala/org/bdgenomics/guacamole/Read.scala
+++ src/main/scala/org/bdgenomics/guacamole/Read.scala
@@ -279,9 +279,9 @@ object Read extends Logging {
 // Serialization: MappedRead
 class MappedReadSerializer extends Serializer[MappedRead] {
   def write(kryo: Kryo, output: Output, obj: MappedRead) = {
+    assert(obj.sequence.length == obj.baseQualities.length)
     output.writeInt(obj.sequence.length, true)
     output.writeBytes(obj.sequence)
-    output.writeInt(obj.baseQualities.length, true)
     output.writeBytes(obj.baseQualities)
     output.writeBoolean(obj.isDuplicate)
     output.writeString(obj.sampleName)
@@ -293,10 +293,9 @@ class MappedReadSerializer extends Serializer[MappedRead] {
     output.writeBoolean(obj.failedVendorQualityChecks)
   }
   def read(kryo: Kryo, input: Input, klass: Class[MappedRead]): MappedRead = {
-    val sequenceCount: Int = input.readInt(true)
-    val sequenceArray: Array[Byte] = input.readBytes(sequenceCount)
-    val qualitiesCount: Int = input.readInt(true)
-    val qualityScoresArray = input.readBytes(qualitiesCount)
+    val count: Int = input.readInt(true)
+    val sequenceArray: Array[Byte] = input.readBytes(count)
+    val qualityScoresArray = input.readBytes(count)
     val isDuplicate = input.readBoolean()
     val sampleName = input.readString().intern()
     val referenceContig = input.readString().intern()
@@ -325,23 +324,21 @@ class MappedReadSerializer extends Serializer[MappedRead] {
 // Serialization: UnmappedRead
 class UnmappedReadSerializer extends Serializer[UnmappedRead] {
   def write(kryo: Kryo, output: Output, obj: UnmappedRead) = {
+    assert(obj.sequence.length == obj.baseQualities.length)
     output.writeInt(obj.sequence.length, true)
     output.writeBytes(obj.sequence)
-    output.writeInt(obj.baseQualities.length, true)
     output.writeBytes(obj.baseQualities)
     output.writeBoolean(obj.isDuplicate)
     output.writeString(obj.sampleName)
     output.writeBoolean(obj.failedVendorQualityChecks)
   }
   def read(kryo: Kryo, input: Input, klass: Class[UnmappedRead]): UnmappedRead = {
-    val sequenceCount: Int = input.readInt(true)
-    val sequenceArray: Array[Byte] = input.readBytes(sequenceCount)
-    val qualitiesCount: Int = input.readInt(true)
-    val qualityScoresArray = input.readBytes(qualitiesCount)
+    val count: Int = input.readInt(true)
+    val sequenceArray: Array[Byte] = input.readBytes(count)
+    val qualityScoresArray = input.readBytes(count)
     val isDuplicate = input.readBoolean()
     val sampleName = input.readString().intern()
     val failedVendorQualityChecks = input.readBoolean()
-
     UnmappedRead(
       sequenceArray,
       qualityScoresArray,
