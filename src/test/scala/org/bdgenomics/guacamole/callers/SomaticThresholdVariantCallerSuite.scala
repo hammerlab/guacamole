e46e25d3bd553efc77f71ff06f05595d17110f24
Ryan Williams <ryan.blake.williams@gmail.com>
2014-06-23 19:06:35 -0400

    remove unnecessary / redundant sparkTest() call
    This was resulting in two SparkContexts being created during this test's
    execution, which was leading to spurious socket-bind exceptions.

--- src/test/scala/org/bdgenomics/guacamole/callers/SomaticThresholdVariantCallerSuite.scala
+++ src/test/scala/org/bdgenomics/guacamole/callers/SomaticThresholdVariantCallerSuite.scala
@@ -6,14 +6,15 @@ import org.apache.commons.io.FileUtils
 import java.io.{ IOError, File }
 
 class SomaticThresholdVariantCallerSuite extends SparkFunSuite {
-  sparkTest("run the caller") {
-    val output = "/tmp/somatic.threshold.chr20.vcf"
-    TestUtil.deleteIfExists(output)
-    val caller = SomaticThresholdVariantCaller
-    caller.run(Array[String](
+  val output = "/tmp/somatic.threshold.chr20.vcf"
+  TestUtil.deleteIfExists(output)
+  val caller = SomaticThresholdVariantCaller
+  caller.run(
+    Array[String](
       "-tumor-reads", TestUtil.testDataPath("synth1.tumor.100k-200k.withmd.bam"),
       "-normal-reads", TestUtil.testDataPath("synth1.normal.100k-200k.withmd.bam"),
       "-parallelism", "20",
-      "-out", output))
-  }
+      "-out", output
+    )
+  )
 }
