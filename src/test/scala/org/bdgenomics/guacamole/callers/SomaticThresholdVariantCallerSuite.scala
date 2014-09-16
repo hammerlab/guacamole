86bfe44a2517decbd31e8547f76e550f1e3fcdc6
Ryan Williams <ryan.blake.williams@gmail.com>
2014-09-15 23:11:07 -0400

    avoid making multiple SparkContexts in tests
    this seems to have been the cause of a crazy nondeterministic
    failure of DistributedUtilSuite.

--- src/test/scala/org/bdgenomics/guacamole/callers/SomaticThresholdVariantCallerSuite.scala
+++ src/test/scala/org/bdgenomics/guacamole/callers/SomaticThresholdVariantCallerSuite.scala
@@ -1,17 +1,26 @@
 package org.bdgenomics.guacamole.callers
 
+import org.bdgenomics.adam.cli.Args4j
+import org.bdgenomics.guacamole.callers.SomaticThresholdVariantCaller.Arguments
 import org.bdgenomics.guacamole.TestUtil.SparkFunSuite
 import org.bdgenomics.guacamole.TestUtil
 
 class SomaticThresholdVariantCallerSuite extends SparkFunSuite {
   val output = "/tmp/somatic.threshold.chr20.vcf"
   TestUtil.deleteIfExists(output)
-  val caller = SomaticThresholdVariantCaller
-  caller.run(
-    Array[String](
-      "-tumor-reads", TestUtil.testDataPath("synth1.tumor.100k-200k.withmd.bam"),
-      "-normal-reads", TestUtil.testDataPath("synth1.normal.100k-200k.withmd.bam"),
-      "-parallelism", "20",
-      "-loci", "20:100000-200000",
-      "-out", output))
+
+  sparkTest("somatic threshold") {
+    SomaticThresholdVariantCaller.runWrapper(
+      sc,
+      Args4j[Arguments](
+        Array[String](
+          "-tumor-reads", TestUtil.testDataPath("synth1.tumor.100k-200k.withmd.bam"),
+          "-normal-reads", TestUtil.testDataPath("synth1.normal.100k-200k.withmd.bam"),
+          "-parallelism", "20",
+          "-loci", "20:100000-200000",
+          "-out", output
+        )
+      )
+    )
+  }
 }
