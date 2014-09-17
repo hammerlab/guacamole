6cfa3223382151ccd613f9d5af1abee3d1304b48
Ryan Williams <ryan.blake.williams@gmail.com>
2014-09-16 22:21:24 -0400

    lots of Bayes' Pileup/Genotype test cleanup
    * remove genotypes that were not being checked
        * will follow on with GenotypeAlleles tests for these
        * assert that we are only checking the genotypes that are returned
    * make the precision 1e-12
        * we were not always checking the right numbers previously, but were still within e.g. 1e-2.
        * fix the numbers we're checking
    * a few general cleanups
        * common reference base / reads across test cases
        * use Table matchers

--- src/test/scala/org/bdgenomics/guacamole/callers/BayesianQualityVariantCallerSuite.scala
+++ src/test/scala/org/bdgenomics/guacamole/callers/BayesianQualityVariantCallerSuite.scala
@@ -5,163 +5,152 @@ import org.bdgenomics.guacamole.variants.GenotypeAlleles
 import org.bdgenomics.guacamole.{ Bases, TestUtil }
 import org.bdgenomics.guacamole.pileup.{ Allele, Pileup }
 import org.bdgenomics.adam.util.PhredUtils
+import org.scalatest.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class BayesianQualityVariantCallerSuite extends SparkFunSuite {
+class BayesianQualityVariantCallerSuite extends SparkFunSuite with TableDrivenPropertyChecks with Matchers {
 
-  def makeGenotype(referenceBase: Char, alleles: String*): GenotypeAlleles = {
+  val referenceBase = 'C'.toByte
+
+  def makeGenotype(alleles: String*): GenotypeAlleles = {
     // If we later change Genotype to work with Array[byte] instead of strings, we can use this function to convert
     // to byte arrays.
-    GenotypeAlleles(alleles.map(allele => Allele(Seq(referenceBase.toByte), Bases.stringToBases(allele))): _*)
+    GenotypeAlleles(alleles.map(allele => Allele(Seq(referenceBase), Bases.stringToBases(allele))): _*)
   }
 
-  val floatingPointingThreshold = 1e-6
   val errorPhred30 = PhredUtils.phredToErrorProbability(30)
   val errorPhred40 = PhredUtils.phredToErrorProbability(40)
 
-  test("score genotype for single sample, all bases ref") {
+  def refRead(phred: Int) = TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(phred)))
+  def altRead(phred: Int) = TestUtil.makeRead("A", "1M", "0C0", 1, "chr1", Some(Array(phred)))
 
-    val reads = Seq(
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(30))),
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(40))),
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(30))))
-
-    val pileup = Pileup(reads, 1)
-    val hetLikelihood = ((1 - errorPhred30) + errorPhred30 / 3) * ((1 - errorPhred40) + errorPhred40 / 3) * ((1 - errorPhred30) + errorPhred30 / 3) / 8.0
-    val altLikelihood = (2 * errorPhred30 / 3) * (2 * errorPhred40 / 3) * (2 * errorPhred30 / 3) / 8.0
-    val expectedLikelihoods = scala.collection.mutable.Map.empty[GenotypeAlleles, Double]
-
-    expectedLikelihoods += makeGenotype('C', "C", "C") -> (2 * ((1 - errorPhred30) * 2 * (1 - errorPhred40) * 2 * (1 - errorPhred30))) / 8.0
-
-    expectedLikelihoods += makeGenotype('C', "A", "C") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('C', "C", "G") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('C', "T", "C") -> hetLikelihood
-
-    expectedLikelihoods += makeGenotype('C', "A", "A") -> altLikelihood
-    expectedLikelihoods += makeGenotype('C', "G", "G") -> altLikelihood
-    expectedLikelihoods += makeGenotype('C', "T", "G") -> altLikelihood
-
-    val scored = pileup.computeLikelihoods().toMap
-    scored.foreach(l => TestUtil.assertAlmostEqual(l._2, expectedLikelihoods(l._1), 1e-2))
+  def testLikelihoods(actualLikelihoods: Seq[(GenotypeAlleles, Double)],
+                      expectedLikelihoods: Map[GenotypeAlleles, Double],
+                      acceptableError: Double = 1e-12): Unit = {
+    actualLikelihoods.size should equal(expectedLikelihoods.size)
+    val actualLikelihoodsMap = actualLikelihoods.toMap
+    forAll(Table("genotype", expectedLikelihoods.toList: _*)) {
+      l => TestUtil.assertAlmostEqual(actualLikelihoodsMap(l._1), l._2, acceptableError)
+    }
   }
 
-  test("score genotype for single sample, mix of ref/non-ref bases") {
+  val homRefLikelihood = ((1 - errorPhred30) * (1 - errorPhred40) * (1 - errorPhred30))
+
+  test("score genotype for single sample; all bases ref") {
+
     val reads = Seq(
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(30))),
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(40))),
-      TestUtil.makeRead("A", "1M", "0C0", 1, "chr1", Some(Array(30))))
+      refRead(30),
+      refRead(40),
+      refRead(30)
+    )
 
     val pileup = Pileup(reads, 1)
 
-    val hetLikelihood = ((1 - errorPhred30) + errorPhred30 / 3) * ((1 - errorPhred40) + errorPhred40 / 3) * ((1 - errorPhred30) + errorPhred30 / 3) / 8.0
-    val expectedLikelihoods = scala.collection.mutable.Map.empty[GenotypeAlleles, Double]
+    val expectedLikelihoods: Map[GenotypeAlleles, Double] =
+      Map(
+        makeGenotype("C", "C") -> homRefLikelihood
+      )
 
-    expectedLikelihoods += makeGenotype('C', "C", "C") -> (2 * ((1 - errorPhred30) * 2 * (1 - errorPhred40) * 2 * errorPhred30)) / 8.0
-    expectedLikelihoods += makeGenotype('C', "A", "C") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('C', "A", "A") -> (2 * errorPhred30 * 2 * errorPhred40 * 2 * (1 - errorPhred30)) / 8.0
-
-    val scored = pileup.computeLikelihoods().toMap
-    scored.foreach(l => TestUtil.assertAlmostEqual(l._2, expectedLikelihoods(l._1), 1e-3))
+    testLikelihoods(pileup.computeLikelihoods(includeAlignmentLikelihood = false), expectedLikelihoods)
   }
 
-  test("score genotype for single sample, all bases non-ref") {
-
+  test("score genotype for single sample; mix of ref/non-ref bases") {
     val reads = Seq(
-      TestUtil.makeRead("A", "1M", "0A0", 1, "chr1", Some(Array(30))),
-      TestUtil.makeRead("A", "1M", "0A0", 1, "chr1", Some(Array(40))),
-      TestUtil.makeRead("A", "1M", "0A0", 1, "chr1", Some(Array(30))))
+      refRead(30),
+      refRead(40),
+      altRead(30)
+    )
 
     val pileup = Pileup(reads, 1)
 
-    val hetLikelihood = ((1 - errorPhred30) + errorPhred30 / 3) * ((1 - errorPhred40) + errorPhred40 / 3) * ((1 - errorPhred30) + errorPhred30 / 3) / 8.0
-    val allErrorLikelihood = (2 * errorPhred30 / 3) * (2 * errorPhred40 / 3) * (2 * errorPhred30 / 3) / 8.0
-    val expectedLikelihoods = scala.collection.mutable.Map.empty[GenotypeAlleles, Double]
+    val expectedLikelihoods: Map[GenotypeAlleles, Double] =
+      Map(
+        makeGenotype("C", "C") -> (1 - errorPhred30) * (1 - errorPhred40) * errorPhred30,
+        makeGenotype("A", "C") -> 1 / 8.0,
+        makeGenotype("A", "A") -> errorPhred30 * errorPhred40 * (1 - errorPhred30)
+      )
 
-    expectedLikelihoods += makeGenotype('A', "A", "A") -> (2 * ((1 - errorPhred30) * 2 * (1 - errorPhred40) * 2 * (1 - errorPhred30))) / 8.0
-
-    expectedLikelihoods += makeGenotype('A', "A", "C") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('A', "A", "G") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('A', "A", "T") -> hetLikelihood
-
-    expectedLikelihoods += makeGenotype('A', "T", "T") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('A', "G", "G") -> hetLikelihood
-
-    expectedLikelihoods += makeGenotype('A', "C", "C") -> allErrorLikelihood
-
-    val scored = pileup.computeLikelihoods().toMap
-    scored.foreach(l => TestUtil.assertAlmostEqual(l._2, expectedLikelihoods(l._1), 1e-2))
+    testLikelihoods(pileup.computeLikelihoods(includeAlignmentLikelihood = false), expectedLikelihoods)
   }
 
-  test("log score genotype for single sample, all bases ref") {
+  test("score genotype for single sample; all bases non-ref") {
 
     val reads = Seq(
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(30))),
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(40))),
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(30))))
+      altRead(30),
+      altRead(40),
+      altRead(30)
+    )
 
     val pileup = Pileup(reads, 1)
-    val hetLikelihood = math.log((1 - errorPhred30) + errorPhred30 / 3) + math.log((1 - errorPhred40) + errorPhred40 / 3) + math.log((1 - errorPhred30) + errorPhred30 / 3) - 3.0
-    val altLikelihood = math.log(2 * errorPhred30 / 3) + math.log(2 * errorPhred40 / 3) + math.log(2 * errorPhred30 / 3) - 3.0
-    val expectedLikelihoods = scala.collection.mutable.Map.empty[GenotypeAlleles, Double]
 
-    expectedLikelihoods += makeGenotype('C', "C", "C") -> (math.log(2 * (1 - errorPhred30)) + math.log(2 * (1 - errorPhred40)) + math.log(2 * (1 - errorPhred30)) - 3)
+    val expectedLikelihoods: Map[GenotypeAlleles, Double] =
+      Map(
+        makeGenotype("A", "A") -> (1 - errorPhred30) * (1 - errorPhred40) * (1 - errorPhred30)
+      )
 
-    expectedLikelihoods += makeGenotype('C', "A", "C") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('C', "C", "G") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('C', "T", "C") -> hetLikelihood
-
-    expectedLikelihoods += makeGenotype('C', "A", "A") -> altLikelihood
-    expectedLikelihoods += makeGenotype('C', "G", "G") -> altLikelihood
-    expectedLikelihoods += makeGenotype('C', "T", "G") -> altLikelihood
-
-    val scored = pileup.computeLogLikelihoods().toMap
-    scored.foreach(l => TestUtil.assertAlmostEqual(l._2, expectedLikelihoods(l._1)))
+    testLikelihoods(pileup.computeLikelihoods(includeAlignmentLikelihood = false), expectedLikelihoods)
   }
 
-  test("log score genotype for single sample, mix of ref/non-ref bases") {
+  test("log score genotype for single sample; all bases ref") {
+
     val reads = Seq(
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(30))),
-      TestUtil.makeRead("C", "1M", "1", 1, "chr1", Some(Array(40))),
-      TestUtil.makeRead("A", "1M", "0C0", 1, "chr1", Some(Array(30))))
+      refRead(30),
+      refRead(40),
+      refRead(30)
+    )
 
     val pileup = Pileup(reads, 1)
 
-    val hetLikelihood = math.log((1 - errorPhred30) + errorPhred30 / 3) + math.log((1 - errorPhred40) + errorPhred40 / 3) + math.log((1 - errorPhred30) + errorPhred30 / 3) - 3.0
-    val expectedLikelihoods = scala.collection.mutable.Map.empty[GenotypeAlleles, Double]
+    val expectedLikelihoods: Map[GenotypeAlleles, Double] =
+      Map(
+        makeGenotype("C", "C") ->
+          (math.log(1 - errorPhred30) + math.log(1 - errorPhred40) + math.log(1 - errorPhred30))
+      )
 
-    expectedLikelihoods += makeGenotype('C', "C", "C") -> (math.log(2 * (1 - errorPhred30)) + math.log(2 * (1 - errorPhred40)) + math.log(2 * errorPhred30) - 3.0)
-    expectedLikelihoods += makeGenotype('C', "A", "C") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('C', "A", "A") -> (math.log(2 * errorPhred30) + math.log(2 * errorPhred40) + math.log(2 * (1 - errorPhred30)) - 3.0)
-
-    val scored = pileup.computeLogLikelihoods().toMap
-    scored.foreach(l => TestUtil.assertAlmostEqual(l._2, expectedLikelihoods(l._1), 1e-2))
+    testLikelihoods(pileup.computeLogLikelihoods(), expectedLikelihoods)
   }
 
-  test("log score genotype for single sample, all bases non-ref") {
-
+  test("log score genotype for single sample; mix of ref/non-ref bases") {
     val reads = Seq(
-      TestUtil.makeRead("A", "1M", "0A0", 1, "chr1", Some(Array(30))),
-      TestUtil.makeRead("A", "1M", "0A0", 1, "chr1", Some(Array(40))),
-      TestUtil.makeRead("A", "1M", "0A0", 1, "chr1", Some(Array(30))))
+      refRead(30),
+      refRead(40),
+      altRead(30)
+    )
 
     val pileup = Pileup(reads, 1)
 
-    val hetLikelihood = math.log((1 - errorPhred30) + errorPhred30 / 3) + math.log((1 - errorPhred40) + errorPhred40 / 3) + math.log((1 - errorPhred30) + errorPhred30 / 3) - 3.0
-    val allErrorLikelihood = math.log(2 * errorPhred30 / 3) + math.log(2 * errorPhred40 / 3) + math.log(2 * errorPhred30 / 3) - 3.0
-    val expectedLikelihoods = scala.collection.mutable.Map.empty[GenotypeAlleles, Double]
+    val expectedLikelihoods: Map[GenotypeAlleles, Double] =
+      Map(
 
-    expectedLikelihoods += makeGenotype('A', "A", "A") -> (math.log(2 * (1 - errorPhred30)) + math.log(2.0 * (1 - errorPhred30)) + math.log(2 * (1 - errorPhred30)) - 3.0)
+        makeGenotype("C", "C") ->
+          (math.log(1 - errorPhred30) + math.log(1 - errorPhred40) + math.log(errorPhred30)),
 
-    expectedLikelihoods += makeGenotype('A', "A", "C") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('A', "A", "G") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('A', "A", "T") -> hetLikelihood
+        makeGenotype("A", "C") -> math.log(1.0 / 8),
 
-    expectedLikelihoods += makeGenotype('A', "T", "T") -> hetLikelihood
-    expectedLikelihoods += makeGenotype('A', "G", "G") -> hetLikelihood
+        makeGenotype("A", "A") ->
+          (math.log(errorPhred30) + math.log(errorPhred40) + math.log(1 - errorPhred30))
+      )
 
-    expectedLikelihoods += makeGenotype('A', "C", "C") -> allErrorLikelihood
+    testLikelihoods(pileup.computeLogLikelihoods(), expectedLikelihoods)
+  }
 
-    val scored = pileup.computeLogLikelihoods().toMap
-    scored.foreach(l => TestUtil.assertAlmostEqual(l._2, expectedLikelihoods(l._1), 1e-2))
+  test("log score genotype for single sample; all bases non-ref") {
+
+    val reads = Seq(
+      altRead(30),
+      altRead(40),
+      altRead(30)
+    )
+
+    val pileup = Pileup(reads, 1)
+
+    val expectedLikelihoods: Map[GenotypeAlleles, Double] =
+      Map(
+        makeGenotype("A", "A") ->
+          (math.log(1 - errorPhred30) + math.log(1 - errorPhred40) + math.log(1 - errorPhred30))
+      )
+
+    testLikelihoods(pileup.computeLogLikelihoods(), expectedLikelihoods)
   }
 
 }
