c09f1c0ce2b09d6623771a0257fda9b4e90a9f52
Ryan Williams <ryan.blake.williams@gmail.com>
2014-09-18 12:21:54 -0400

    make Allele extend Ordered

--- src/main/scala/org/bdgenomics/guacamole/variants/Allele.scala
+++ src/main/scala/org/bdgenomics/guacamole/variants/Allele.scala
@@ -3,12 +3,12 @@ package org.bdgenomics.guacamole.variants
 import org.bdgenomics.guacamole.Bases
 import org.bdgenomics.guacamole.Bases.BasesOrdering
 
-case class Allele(refBases: Seq[Byte], altBases: Seq[Byte]) {
+case class Allele(refBases: Seq[Byte], altBases: Seq[Byte]) extends Ordered[Allele] {
   lazy val isVariant = BasesOrdering.compare(refBases, altBases) != 0
 
   override def toString: String = "Allele(%s,%s)".format(Bases.basesToString(refBases), Bases.basesToString(altBases))
 
-  def equals(other: Allele): Boolean = AlleleOrdering.compare(this, other) == 0
-  def ==(other: Allele): Boolean = equals(other)
+  override def compare(that: Allele): Int = AlleleOrdering.compare(this, that)
+  def ==(other: Allele): Boolean = compare(other) == 0
 }
 
