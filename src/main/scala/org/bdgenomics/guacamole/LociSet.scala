577b67d47d82906217e15f4fc6d979a33c7f948a
Tim O'Donnell <timodonnell@gmail.com>
2014-11-23 13:08:27 -0500

    Add LociSet.SingleContig.Iterator class
    Implements an iterator for loci on a single contig, and adds tests for it.

--- src/main/scala/org/bdgenomics/guacamole/LociSet.scala
+++ src/main/scala/org/bdgenomics/guacamole/LociSet.scala
@@ -171,7 +171,7 @@ object LociSet {
     def isEmpty(): Boolean = map.isEmpty
 
     /** Iterator through loci in this set, sorted. */
-    def individually(): Iterator[Long] = map.lociIndividually()
+    def iterator(): SingleContig.Iterator = new SingleContig.Iterator(this)
 
     /** Returns the union of this set with another. Both must be on the same contig. */
     def union(other: SingleContig): SingleContig = SingleContig(map.union(other.map))
@@ -183,6 +183,95 @@ object LociSet {
 
     /** String representation, truncated to maxLength characters. */
     def truncatedString(maxLength: Int = 100): String = map.truncatedString(maxLength, false)
+  }
+  object SingleContig {
+
+    /**
+     * An iterator over loci on a single contig. Loci from this iterator are sorted (monotonically increasing).
+     *
+     * This can be used as a plain scala Iterator[Long], but also supports extra functionality for quickly skipping
+     * ahead past a given locus.
+     *
+     * @param loci loci to iterate over
+     */
+    class Iterator(loci: SingleContig) extends scala.Iterator[Long] {
+      private var ranges = loci.ranges.iterator
+
+      /** The range for the *next* locus to be returned. */
+      var headRangeOption: Option[SimpleRange] = if (ranges.isEmpty) None else Some(ranges.next())
+
+      /** The index in the range for the next element to be returned. */
+      var headIndex = 0L
+
+      /** true if calling next() will succeed. */
+      def hasNext() = headRangeOption.nonEmpty
+
+      /**
+       * Advance the iterator and return the current head.
+       *
+       * Throws NoSuchElementException if the iterator is already at the end.
+       */
+      def next(): Long = {
+        if (!hasNext) {
+          throw new NoSuchElementException("next on empty Loci iterator")
+        }
+        val nextLocus: Long = headOption.get
+
+        // Advance
+        headIndex += 1
+        if (headIndex == headRangeOption.get.length) {
+          nextRange()
+        }
+        nextLocus
+      }
+
+      /**
+       * Skip ahead to the first locus in the iterator that is at or past the given locus.
+       *
+       * After calling this, a subsequent call to next() will return the first element in the iterator that is >= the
+       * given locus. If there is no such element, then the iterator will be empty after calling this method.
+       *
+       */
+      def skipTo(locus: Long): Unit = {
+        // Skip entire ranges until we hit one whose end is past the target locus.
+        while (headRangeOption.exists(_.end <= locus)) {
+          nextRange()
+        }
+        // If we're not at the end of the iterator and the current head range includes the target locus, set our index
+        // so that our next locus is the target.
+        headRangeOption match {
+          case Some(range) if (locus >= range.start && locus < range.end) => {
+            headIndex = locus - range.start
+          }
+          case _ => {}
+        }
+      }
+
+      /**
+       * Skip ahead to the end of the iterator. After calling this, the iterator will be empty.
+       */
+      def skipToEnd(): Unit = {
+        ranges = Iterator.empty
+        headRangeOption = None
+      }
+
+      /**
+       * The next locus in the iterator, i.e. what the next call to next() will return.
+       *
+       * Some(locus) if there is a next element, None otherwise.
+       */
+      def headOption(): Option[Long] =
+        headRangeOption.map(_.start + headIndex)
+
+      /**
+       * Advance past all loci in the current head range. Return the next range if one exists.
+       */
+      def nextRange(): Option[SimpleRange] = {
+        headIndex = 0
+        headRangeOption = if (ranges.hasNext) Some(ranges.next()) else None
+        headRangeOption
+      }
+    }
   }
 }
 
--- src/test/scala/org/bdgenomics/guacamole/LociSetSuite.scala
+++ src/test/scala/org/bdgenomics/guacamole/LociSetSuite.scala
@@ -67,7 +67,7 @@ class LociSetSuite extends TestUtil.SparkFunSuite with Matchers {
     set.onContig("chr21").intersects(90, 100) should be(false)
     set.onContig("chr21").intersects(90, 101) should be(true)
     set.onContig("chr21").intersects(90, 95) should be(false)
-    set.onContig("chr21").individually.toSeq should equal(100 until 200)
+    set.onContig("chr21").iterator.toSeq should equal(100 until 200)
   }
 
   sparkTest("loci set invariants") {
@@ -90,7 +90,7 @@ class LociSetSuite extends TestUtil.SparkFunSuite with Matchers {
         set should not equal (set.union(LociSet.parse("abc123:30-40")))
         set should equal(set.union(LociSet.parse("empty:99-99")))
 
-        // Test serialization. We hit all sorts of null pointer exceptions here at once point, so we are paranoid about
+        // Test serialization. We hit all sorts of null pointer exceptions here at one point, so we are paranoid about
         // checking every pointer.
         assert(sc != null)
         val parallelized = sc.parallelize(List(set))
@@ -151,6 +151,86 @@ class LociSetSuite extends TestUtil.SparkFunSuite with Matchers {
       set.onContig("20").toString
     }).collect.toSeq
     result should equal(sets.map(_.onContig("20").toString).toSeq)
+  }
+
+  sparkTest("loci set union") {
+    val set1 = LociSet.parse("chr1:40-43")
+    val set2 = LociSet.parse("chr1:40-42")
+    set1.union(set2).toString should equal("chr1:40-43")
+  }
+
+  sparkTest("loci set single contig iterator basic") {
+    val set = LociSet.parse("chr1:20-25,chr1:15-17,chr1:40-43,chr1:40-42,chr1:5-5,chr2:5-6,chr2:6-7,chr2:2-4")
+    set.onContig("chr1").iterator.toSeq should equal(Seq(15, 16, 20, 21, 22, 23, 24, 40, 41, 42))
+    set.onContig("chr2").iterator.toSeq should equal(Seq(2, 3, 5, 6))
+
+    val iter1 = set.onContig("chr1").iterator
+    iter1.hasNext should be(true)
+    iter1.headOption.get should be(15)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(15, 17))
+    iter1.headIndex should be(0)
+    iter1.next() should be(15)
+    iter1.headOption.get should be(16)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(15, 17))
+    iter1.headIndex should be(1)
+    iter1.next() should be(16)
+    iter1.headOption.get should be(20)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(20, 25))
+    iter1.headIndex should be(0)
+    iter1.next() should be(20)
+    iter1.headOption.get should be(21)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(20, 25))
+    iter1.headIndex should be(1)
+    iter1.skipTo(23)
+    iter1.next() should be(23)
+    iter1.headOption.get should be(24)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(20, 25))
+    iter1.headIndex should be(4)
+    iter1.skipTo(5)
+    iter1.headOption.get should be(24)
+    iter1.skipTo(38)
+    iter1.headOption.get should be(40)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(40, 43))
+    iter1.headIndex should be(0)
+    iter1.hasNext should be(true)
+    iter1.skipTo(100)
+    iter1.headOption should be(None)
+    iter1.headRangeOption should be(None)
+    iter1.hasNext should be(false)
+
+    val iter2 = set.onContig("chr1").iterator
+    iter2.skipToEnd()
+    iter2.headOption should be(None)
+    iter2.headRangeOption should be(None)
+    iter2.hasNext should be(false)
+  }
+
+  sparkTest("loci set single contig iterator: test that skipTo implemented efficiently.") {
+    val set = LociSet.parse("chr1:2-3,chr1:10-15,chr1:100-100000000000")
+
+    val iter1 = set.onContig("chr1").iterator
+    iter1.hasNext should be(true)
+    iter1.headOption.get should be(2)
+    iter1.headRangeOption.get should equal(LociMap.SimpleRange(2, 3))
+    iter1.headIndex should be(0)
+    iter1.next() should be(2)
+    iter1.next() should be(10)
+    iter1.next() should be(11)
+    iter1.skipTo(6000000000L) // will hang if it steps through each locus.
+    iter1.next() should be(6000000000L)
+    iter1.next() should be(6000000001L)
+    iter1.skipToEnd()
+    iter1.hasNext() should be(false)
+
+    val iter2 = set.onContig("chr1").iterator
+    iter2.skipTo(100000000000L)
+    iter2.hasNext() should be(false)
+
+    val iter3 = set.onContig("chr1").iterator
+    iter3.skipTo(100000000000L - 1L)
+    iter3.hasNext() should be(true)
+    iter3.next() should be(100000000000L - 1L)
+    iter3.hasNext() should be(false)
   }
 
   // This test currently fails, because we do not provide java serialization for LociSet. Instead of 
