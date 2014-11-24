6ca2f20ae960e4cc7a0a91aeee0d4bad50ba73cb
Tim O'Donnell <timodonnell@gmail.com>
2014-11-24 16:42:41 -0500

    Tweak documentation in windowing package.

--- src/main/scala/org/bdgenomics/guacamole/windowing/package.scala
+++ src/main/scala/org/bdgenomics/guacamole/windowing/package.scala
@@ -1,7 +1,7 @@
 package org.bdgenomics.guacamole
 
 /**
- * Step through loci in the genome and at each step consider all the reads (or other read-like objects) that overlap the
- * locus.
+ * Step through loci in the genome and at each step consider all the reads (or any object that implements the trait
+ * [[HasReferenceRegion]]) that overlap the locus.
  */
 package object windowing {}
