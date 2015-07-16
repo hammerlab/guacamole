a36816c5719e570fe530e9d58c93a5de8860605a
Tim O'Donnell <timodonnell@gmail.com>
2015-07-16 15:45:00 -0400

    start multi sample caller

---
+++ src/main/scala/org/hammerlab/guacamole/likelihood/PValue.scala
@@ -0,0 +1,15 @@
+package org.hammerlab.guacamole.likelihood
+
+import org.hammerlab.guacamole.pileup.Pileup
+import org.hammerlab.guacamole.variants.Genotype
+
+object PValue {
+
+  def pValue(genotype: Genotype, pileup: Pileup): Double = {
+    // weight reads by quality, to derive an adjusted "num matching" and "num nonmatching"
+    // compute p value of observing the read counts drawn from a population of the given
+    // genotype plus an error population. (e.g. fischer's exact test).
+
+    genotype.alleles
+  }
+}
