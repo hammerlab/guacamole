582adca718a0cd813cff8b5968b19a05031d2a59
Arun Ahuja <aahuja11@gmail.com>
2015-07-26 15:52:19 -0400

    add as possible output

--- src/main/scala/org/hammerlab/guacamole/variants/ReferenceVariant.scala
+++ src/main/scala/org/hammerlab/guacamole/variants/ReferenceVariant.scala
@@ -18,8 +18,8 @@
 
 package org.hammerlab.guacamole.variants
 
-import org.bdgenomics.formats.avro.{ Contig, Variant }
-import org.hammerlab.guacamole.{ Bases, HasReferenceRegion }
+import org.bdgenomics.formats.avro.{DatabaseVariantAnnotation, Contig, Variant}
+import org.hammerlab.guacamole.{Bases, HasReferenceRegion}
 
 /**
  * Base properties of a genomic change in a sequence sample from a reference genome
@@ -48,4 +48,10 @@ trait ReferenceVariant extends HasReferenceRegion {
     .build
 
   val rsID: Option[Int]
+
+  def adamVariantDatabase = {
+    val builder = DatabaseVariantAnnotation.newBuilder
+    rsID.foreach(builder.setDbSnpId(_))
+    builder.build
+  }
 }
