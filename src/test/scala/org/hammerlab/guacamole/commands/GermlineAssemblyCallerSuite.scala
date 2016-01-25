dac38a86605fb7879beec2da9d5f8377f5d86daa
Arun Ahuja <aahuja11@gmail.com>
2016-01-25 17:12:26 -0500

    update test with illumina platinum test

--- src/test/scala/org/hammerlab/guacamole/commands/GermlineAssemblyCallerSuite.scala
+++ src/test/scala/org/hammerlab/guacamole/commands/GermlineAssemblyCallerSuite.scala
@@ -1,24 +1,11 @@
 package org.hammerlab.guacamole.commands
 
+import org.bdgenomics.adam.rdd.ADAMContext
 import org.hammerlab.guacamole.commands.GermlineAssemblyCaller.Arguments
-import org.hammerlab.guacamole.util.{ TestUtil, GuacFunSuite }
+import org.hammerlab.guacamole.util.{GuacFunSuite, TestUtil}
+import org.scalatest.Matchers
 
-class GermlineAssemblyCallerSuite extends GuacFunSuite {
-
-  //  sparkTest("test assembly caller") {
-  //    val input = "assemble-reads-set3-chr2-73613071.sam"
-  //
-  //
-  //    val args = new Arguments
-  //    args.reads = TestUtil.testDataPath(input)
-  //    args.referenceFastaPath = TestUtil.testDataPath("chr2.fasta")
-  //    args.parallelism = 2
-  //    args.variantOutput = TestUtil.tmpFileName(suffix = ".vcf")
-  //    args.snvWindowRange = 1000
-  //
-  //    GermlineAssemblyCaller.Caller.run(args, sc)
-  //
-  //  }
+class GermlineAssemblyCallerSuite extends GuacFunSuite with Matchers {
 
   sparkTest("test assembly caller: illumina platinum tests") {
     val input = "illumina-platinum-na12878/NA12878.10k_variants.plus_chr1_3M-3.1M.bam"
@@ -30,9 +17,18 @@ class GermlineAssemblyCallerSuite extends GuacFunSuite {
     args.parallelism = 2
     args.variantOutput = TestUtil.tmpFileName(suffix = ".vcf")
     args.snvWindowRange = 50
-    args.kmerSize = 35
+    args.kmerSize = 45
 
     GermlineAssemblyCaller.Caller.run(args, sc)
+
+    val variants = new ADAMContext(sc).loadVariants(args.variantOutput).collect()
+
+    variants.length should be(1)
+    val variant = variants(0)
+    variant.getContig.getContigName should be ("chr1")
+    variant.getStart should be(772754)
+    variant.getReferenceAllele should be("A")
+    variant.getAlternateAllele should be("C")
 
   }
 
