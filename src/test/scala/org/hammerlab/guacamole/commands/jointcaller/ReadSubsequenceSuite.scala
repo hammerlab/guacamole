8a30fd2f3144be86c7f4f7c85f17df4e8065a708
Ryan Williams <ryan.blake.williams@gmail.com>
2016-03-31 23:25:29 +0000

    style nits

--- src/test/scala/org/hammerlab/guacamole/commands/jointcaller/ReadSubsequenceSuite.scala
+++ src/test/scala/org/hammerlab/guacamole/commands/jointcaller/ReadSubsequenceSuite.scala
@@ -1,10 +1,8 @@
 package org.hammerlab.guacamole.commands.jointcaller
 
-import org.hammerlab.guacamole.Bases
 import org.hammerlab.guacamole.pileup.Pileup
 import org.hammerlab.guacamole.reference.ReferenceBroadcast
-import org.hammerlab.guacamole.reference.ReferenceBroadcast.ArrayBackedReferenceSequence
-import org.hammerlab.guacamole.util.{ TestUtil, GuacFunSuite }
+import org.hammerlab.guacamole.util.{ GuacFunSuite, TestUtil }
 import org.scalatest.Matchers
 
 class ReadSubsequenceSuite extends GuacFunSuite with Matchers {
@@ -19,7 +17,6 @@ class ReadSubsequenceSuite extends GuacFunSuite with Matchers {
   }
 
   sparkTest("ofFixedReferenceLength") {
-    val ref = ArrayBackedReferenceSequence(sc, "NTCGATCGA")
     val reads = Seq(
       TestUtil.makeRead("TCGATCGA", "8M", 1), // no variant
       TestUtil.makeRead("TCGACCCTCGA", "4M3I4M", 1), // insertion
@@ -35,7 +32,6 @@ class ReadSubsequenceSuite extends GuacFunSuite with Matchers {
   }
 
   sparkTest("ofNextAltAllele") {
-    val ref = ArrayBackedReferenceSequence(sc, "NTCGATCGA")
     val reads = Seq(
       TestUtil.makeRead("TCGATCGA", "8M", 1), // no variant
       TestUtil.makeRead("TCGAGCGA", "8M", 1), // snv
@@ -70,7 +66,6 @@ class ReadSubsequenceSuite extends GuacFunSuite with Matchers {
     val pileups = cancerWGS1Bams.map(
       path => TestUtil.loadPileup(sc, path, contig = Some(contigLocus._1), locus = contigLocus._2, reference = partialReference))
 
-    val reference = partialReference
     val subsequences = ReadSubsequence.nextAlts(pileups(1).elements)
     assert(subsequences.nonEmpty)
 
