41c6454735f691ef88c87178e658d0797296411d
Ryan Williams <ryan.blake.williams@gmail.com>
2016-04-10 04:20:49 +0000

    move kryo registrator to kryo package

--- src/test/scala/org/hammerlab/guacamole/util/GuacFunSuite.scala
+++ src/test/scala/org/hammerlab/guacamole/util/GuacFunSuite.scala
@@ -8,7 +8,7 @@ trait GuacFunSuite extends SparkFunSuite with Matchers {
   override val properties: Map[String, String] =
     Map(
       "spark.serializer" -> "org.apache.spark.serializer.KryoSerializer",
-      "spark.kryo.registrator" -> "org.hammerlab.guacamole.GuacamoleKryoRegistrator",
+      "spark.kryo.registrator" -> "org.hammerlab.guacamole.kryo.GuacamoleKryoRegistrator",
       "spark.kryoserializer.buffer" -> "4",
       "spark.kryo.referenceTracking" -> "true"
     )
