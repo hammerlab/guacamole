a4219993c97dd16bcad9105ab97c6928aec968ee
Ryan Williams <ryan.blake.williams@gmail.com>
2016-04-13 14:57:15 +0000

    LociMap: generalize serde to all types

--- src/main/scala/org/hammerlab/guacamole/loci/map/ContigSerializer.scala
+++ src/main/scala/org/hammerlab/guacamole/loci/map/ContigSerializer.scala
@@ -3,26 +3,27 @@ package org.hammerlab.guacamole.loci.map
 import com.esotericsoftware.kryo.io.{Input, Output}
 import com.esotericsoftware.kryo.{Kryo, Serializer => KryoSerializer}
 
-class ContigLongSerializer extends KryoSerializer[Contig[Long]] {
-  def write(kryo: Kryo, output: Output, obj: Contig[Long]) = {
+class ContigSerializer[T] extends KryoSerializer[Contig[T]] {
+  def write(kryo: Kryo, output: Output, obj: Contig[T]) = {
     output.writeString(obj.contig)
     output.writeLong(obj.asMap.size)
     obj.asMap.foreach {
       case (range, value) => {
         output.writeLong(range.start)
         output.writeLong(range.end)
-        output.writeLong(value)
+        kryo.writeClassAndObject(output, value)
       }
     }
   }
-  def read(kryo: Kryo, input: Input, klass: Class[Contig[Long]]): Contig[Long] = {
-    val builder = LociMap.newBuilder[Long]()
+
+  def read(kryo: Kryo, input: Input, klass: Class[Contig[T]]): Contig[T] = {
+    val builder = LociMap.newBuilder[T]()
     val contig = input.readString()
     val count = input.readLong()
     (0L until count).foreach(_ => {
       val start = input.readLong()
       val end = input.readLong()
-      val value = input.readLong()
+      val value: T = kryo.readClassAndObject(input).asInstanceOf[T]
       builder.put(contig, start, end, value)
     })
     builder.result.onContig(contig)
