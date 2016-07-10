e53d002a968bf4d9e7551a0750eabd53a7222655
Ryan Williams <ryan.blake.williams@gmail.com>
2016-07-10 18:33:51 +0000

    LoggingUtils improvements

--- src/main/scala/org/hammerlab/guacamole/logging/LoggingUtils.scala
+++ src/main/scala/org/hammerlab/guacamole/logging/LoggingUtils.scala
@@ -8,15 +8,17 @@ object LoggingUtils {
   /**
    * Print or log a progress message. For now, we just print to standard out, since ADAM's logging setup makes it
    * difficult to see log messages at the INFO level without flooding ourselves with parquet messages.
-   * @param message String to print or log.
+   * @param messages Strings to print or log, one per line.
    */
-  def progress(message: String): Unit = {
+  def progress(messages: String*): Unit = {
     val current = System.currentTimeMillis
-    val time = if (lastProgressTime == 0)
-      java.util.Calendar.getInstance.getTime.toString
-    else
-      "%.2f sec. later".format((current - lastProgressTime) / 1000.0)
-    println("--> [%15s]: %s".format(time, message))
+    val time =
+      if (lastProgressTime == 0)
+        java.util.Calendar.getInstance.getTime.toString
+      else
+        "%.2f sec. later".format((current - lastProgressTime) / 1000.0)
+
+    println("--> [%15s]: %s".format(time, messages.mkString("\n")))
     System.out.flush()
     lastProgressTime = current
   }
