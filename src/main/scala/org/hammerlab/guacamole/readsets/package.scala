3730243414052e7c9869cfa5e0877c8b020a2be6
Ryan Williams <ryan.blake.williams@gmail.com>
2016-07-11 02:21:49 +0000

    Readsets pkg comment

--- src/main/scala/org/hammerlab/guacamole/readsets/package.scala
+++ src/main/scala/org/hammerlab/guacamole/readsets/package.scala
@@ -4,15 +4,7 @@ import org.hammerlab.guacamole.reference.Contig
 import org.hammerlab.guacamole.reference.Position.NumLoci
 
 /**
- * Guacamole is a framework for writing variant callers on the Apache Spark platform. Several variant callers are
- * implemented in the [[org.hammerlab.guacamole.commands]] package. The remaining packages implement a library of
- * functionality used by these callers.
- *
- * To get started, take a look at the code for [[org.hammerlab.guacamole.commands.GermlineThreshold]] for a
- * pedagogical example of a variant caller, then see [[org.hammerlab.guacamole.pileup.Pileup]] to understand how we
- * work with pileups. The [[org.hammerlab.guacamole.commands.SomaticStandard]] caller is a more
- * sophisticated caller (for the somatic setting) that gives an example of most of the functionality in the rest of the
- * library.
+ * Convenience types for loading in sets of reads.
  */
 package object readsets {
   type PerSample[A] = IndexedSeq[A]
