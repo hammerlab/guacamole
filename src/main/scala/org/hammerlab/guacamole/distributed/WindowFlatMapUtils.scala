28a3e4c7f4fa960e90a5160077c79e893d21c17c
Ryan Williams <ryan.blake.williams@gmail.com>
2016-07-24 22:42:50 +0000

    WFMU: split read-partitioning code out

--- src/main/scala/org/hammerlab/guacamole/distributed/WindowFlatMapUtils.scala
+++ src/main/scala/org/hammerlab/guacamole/distributed/WindowFlatMapUtils.scala
@@ -111,6 +111,83 @@ object WindowFlatMapUtils {
     )
   }
 
+  private[distributed] def partitionRegions[R <: ReferenceRegion: ClassTag](
+    regionRDDs: PerSample[RDD[R]],
+    lociPartitionsBoxed: Broadcast[LociPartitioning],
+    halfWindowSize: Int): RDD[(Int, R)] = {
+
+    val sc = regionRDDs(0).sparkContext
+
+    val lociPartitions = lociPartitionsBoxed.value
+
+    progress(s"Partitioning reads according to loci partitioning:\n$lociPartitions")
+
+    val numTasks = lociPartitions.inverse.map(_._1).max + 1
+
+    // Counters
+    val totalRegions = sc.accumulator(0L)
+    val relevantRegions = sc.accumulator(0L)
+    val expandedRegions = sc.accumulator(0L)
+    
+    DelayedMessages.default.say { () =>
+      "Region counts: filtered %,d total regions to %,d relevant regions, expanded for overlaps by %,.2f%% to %,d".format(
+        totalRegions.value,
+        relevantRegions.value,
+        (expandedRegions.value - relevantRegions.value) * 100.0 / relevantRegions.value,
+        expandedRegions.value)
+    }
+
+    // Expand regions into (task, region) pairs for each region RDD.
+    val taskNumberRegionPairsRDDs: PerSample[RDD[(TaskPosition, R)]] =
+      regionRDDs.map(
+        _.flatMap(region => {
+          val singleContig = lociPartitionsBoxed.value.onContig(region.contigName)
+          val partitionsForRegion = singleContig.getAll(region.start - halfWindowSize, region.end + halfWindowSize)
+
+          // Update counters
+          totalRegions += 1
+          if (partitionsForRegion.nonEmpty) relevantRegions += 1
+          expandedRegions += partitionsForRegion.size
+
+          // Return this region, duplicated for each task it is assigned to.
+          partitionsForRegion.map(task => TaskPosition(task, region.contigName, region.start) -> region)
+        })
+      )
+
+    // Run the task on each partition. Keep track of the number of regions assigned to each task in an accumulator, so
+    // we can print out a summary of the skew.
+    val regionsByTask = sc.accumulator(MutableHashMap.empty[String, Long])(new HashMapAccumulatorParam)
+    DelayedMessages.default.say {
+      () => {
+        val stats = new math3.stat.descriptive.DescriptiveStatistics()
+        regionsByTask.value.valuesIterator.foreach(stats.addValue(_))
+        "Regions per task: min=%,.0f 25%%=%,.0f median=%,.0f (mean=%,.0f) 75%%=%,.0f max=%,.0f. Max is %,.2f%% more than mean.".format(
+          stats.getMin,
+          stats.getPercentile(25),
+          stats.getPercentile(50),
+          stats.getMean,
+          stats.getPercentile(75),
+          stats.getMax,
+          ((stats.getMax - stats.getMean) * 100.0) / stats.getMean)
+      }
+    }
+
+    // Build an RDD of (read set num, read), take union of this over all RDDs, and partition by task.
+    sc
+      .union(
+        for {
+          (keyedRegionsRDD, rddIndex) <- taskNumberRegionPairsRDDs.zipWithIndex
+        } yield {
+          for {
+            (taskPosition, read) <- keyedRegionsRDD
+          } yield
+            taskPosition -> (rddIndex, read)
+        }
+      )
+      .repartitionAndSortWithinPartitions(KeyPartitioner(numTasks))
+      .values
+  }
+
   /**
    * FlatMap across sets of regions (e.g. reads) overlapping genomic partitions, on multiple RDDs.
    *
@@ -145,73 +222,23 @@ object WindowFlatMapUtils {
 
     val numRDDs = regionRDDs.length
     assume(numRDDs > 0)
+
     val sc = regionRDDs(0).sparkContext
-    progress(s"Loci partitioning: $lociPartitions")
+
     val lociPartitionsBoxed: Broadcast[LociPartitioning] = sc.broadcast(lociPartitions)
-    val numTasks = lociPartitions.inverse.map(_._1).max + 1
 
-    // Counters
-    val totalRegions = sc.accumulator(0L)
-    val relevantRegions = sc.accumulator(0L)
-    val expandedRegions = sc.accumulator(0L)
-    DelayedMessages.default.say { () =>
-      "Region counts: filtered %,d total regions to %,d relevant regions, expanded for overlaps by %,.2f%% to %,d".format(
-        totalRegions.value,
-        relevantRegions.value,
-        (expandedRegions.value - relevantRegions.value) * 100.0 / relevantRegions.value,
-        expandedRegions.value)
-    }
-
-    // Expand regions into (task, region) pairs for each region RDD.
-    val taskNumberRegionPairsRDDs: PerSample[RDD[(TaskPosition, R)]] =
-      regionRDDs.map(_.flatMap(region => {
-        val singleContig = lociPartitionsBoxed.value.onContig(region.contigName)
-        val thisRegionsTasks = singleContig.getAll(region.start - halfWindowSize, region.end + halfWindowSize)
-
-        // Update counters
-        totalRegions += 1
-        if (thisRegionsTasks.nonEmpty) relevantRegions += 1
-        expandedRegions += thisRegionsTasks.size
-
-        // Return this region, duplicated for each task it is assigned to.
-        thisRegionsTasks.map(task => (TaskPosition(task, region.contigName, region.start), region))
-      }))
-
-    // Run the task on each partition. Keep track of the number of regions assigned to each task in an accumulator, so
-    // we can print out a summary of the skew.
-    val regionsByTask = sc.accumulator(MutableHashMap.empty[String, Long])(new HashMapAccumulatorParam)
-    DelayedMessages.default.say {
-      () => {
-        val stats = new math3.stat.descriptive.DescriptiveStatistics()
-        regionsByTask.value.valuesIterator.foreach(stats.addValue(_))
-        "Regions per task: min=%,.0f 25%%=%,.0f median=%,.0f (mean=%,.0f) 75%%=%,.0f max=%,.0f. Max is %,.2f%% more than mean.".format(
-          stats.getMin,
-          stats.getPercentile(25),
-          stats.getPercentile(50),
-          stats.getMean,
-          stats.getPercentile(75),
-          stats.getMax,
-          ((stats.getMax - stats.getMean) * 100.0) / stats.getMean)
-      }
-    }
+    val partitionedRegions = partitionRegions(regionRDDs, lociPartitionsBoxed, halfWindowSize)
 
     // Accumulator to track the number of loci in each task
     val lociAccumulator = sc.accumulator[Long](0, "NumLoci")
 
-    // Build an RDD of (read set num, read), take union of this over all RDDs, and partition by task.
-    val partitioned = sc.union(
-      taskNumberRegionPairsRDDs.zipWithIndex.map({
-        case (taskNumberRegionPairs, rddIndex: Int) => {
-          taskNumberRegionPairs.map(pair => (pair._1, (rddIndex, pair._2)))
-        }
-      })).repartitionAndSortWithinPartitions(KeyPartitioner(numTasks)).map(_._2)
-
-    partitioned.mapPartitionsWithIndex((taskNum, values) => {
-      val iterators = SplitIterator.split(numRDDs, values)
-      val taskLoci = lociPartitionsBoxed.value.inverse(taskNum)
-      lociAccumulator += taskLoci.count
-      function(taskNum, taskLoci, iterators)
-    })
+    partitionedRegions
+      .mapPartitionsWithIndex((partitionIdx, keyedReads) => {
+        val iterators = SplitIterator.split(numRDDs, keyedReads)
+        val taskLoci = lociPartitionsBoxed.value.inverse(partitionIdx)
+        lociAccumulator += taskLoci.count
+        function(partitionIdx, taskLoci, iterators)
+      })
   }
 
   /**
