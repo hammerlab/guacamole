7e38ee0ca6ad076870b29b55f518330362933b51
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-15 21:35:33 +0000

    rm redundant/obsolete licensing info

--- src/main/scala/org/hammerlab/guacamole/reference/ReferenceRegion.scala
+++ src/main/scala/org/hammerlab/guacamole/reference/ReferenceRegion.scala
@@ -1,21 +1,3 @@
-/**
- * Licensed to Big Data Genomics (BDG) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The BDG licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.hammerlab.guacamole.reference
 
 /**
@@ -36,12 +18,6 @@ trait ReferenceRegion
     start - halfWindowSize <= locus && end + halfWindowSize > locus
   }
 
-  /**
-   * Does the region overlap another reference region
-   *
-   * @param other another region on the genome
-   * @return True if the the regions overlap
-   */
   def overlaps(other: ReferenceRegion): Boolean = {
     other.contigName == contigName && (overlapsLocus(other.start) || other.overlapsLocus(start))
   }
