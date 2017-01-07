f38f0baa05ce41efce6ca687e10539986c5b4b29
Ryan Williams <ryan.blake.williams@gmail.com>
2017-01-07 21:52:03 +0000

    spark serde test

--- src/test/scala/org/hammerlab/guacamole/util/GuacFunSuite.scala
+++ src/test/scala/org/hammerlab/guacamole/util/GuacFunSuite.scala
@@ -1,11 +1,11 @@
 package org.hammerlab.guacamole.util
 
 import org.hammerlab.guacamole.kryo.Registrar
-import org.hammerlab.spark.test.suite.KryoSparkSuite
+import org.hammerlab.spark.test.suite.{ KryoSparkSuite, SparkSerialization }
 
 class GuacFunSuite
   extends KryoSparkSuite(classOf[Registrar], referenceTracking = true)
-    with SparkSerializerSuite {
+    with SparkSerialization {
   conf.setAppName("guacamole")
 }
 
