330e0a09031c598cec057409f8bb6663fe68d4b0
Ryan Williams <ryan.blake.williams@gmail.com>
2017-03-07 00:17:33 +0000

    un-Ordering Allele

--- src/main/scala/org/hammerlab/guacamole/variants/Allele.scala
+++ src/main/scala/org/hammerlab/guacamole/variants/Allele.scala
@@ -1,24 +1,18 @@
 package org.hammerlab.guacamole.variants
 
-import org.hammerlab.genomics.bases.Bases.BasesOrdering
 import org.hammerlab.genomics.bases.{ Base, Bases }
 
 case class Allele(refBases: Bases,
-                  altBases: Bases)
-  extends Ordered[Allele] {
+                  altBases: Bases) {
 
   val isVariant = refBases != altBases
 
   override def toString: String = s"Allele($refBases,$altBases)"
-
-  override def compare(that: Allele): Int =
-    BasesOrdering.compare(refBases, that.refBases) match {
-      case 0 => BasesOrdering.compare(altBases, that.altBases)
-      case x => x
-    }
 }
 
 object Allele {
   def apply(refBase: Base, altBase: Base): Allele =
     Allele(Bases(refBase), Bases(altBase))
+
+  implicit val ordering = Ordering.by(unapply)
 }
